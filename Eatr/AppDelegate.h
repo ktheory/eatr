44e7142464613c34b888484242f1212cc51b0b2a
Aaron Suggs <aaron@ktheory.com>
2013-09-27 16:22:43 -0400

    Initial Commit

---
+++ Eatr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Eatr
+//
+//  Created by Aaron Suggs on 9/27/13.
+//  Copyright (c) 2013 Aaron Suggs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
